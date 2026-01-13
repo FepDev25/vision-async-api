ec653d9162a67737c9ed489602333e98c96d86b2
Felipe <lamediatricolor@gmail.com>
2026-01-13 11:35:56 -0500

    Ft: Celery app object

---
+++ app/core/celery_app.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from app.core.config import settings
+
+# instanciar Celery
+celery_app = Celery(
+    "app",
+    broker=settings.REDIS_URL,
+    backend=settings.REDIS_URL,
+    broker_connection_retry_on_startup=True
+)
+
+# configuración adicional
+celery_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
+)
